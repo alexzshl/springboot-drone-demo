cf0632df5997235a758aaf3e739bd724028fbe2b
zhangdx <zhangdxchn@gmail.com>
2020-02-06 17:35:32 +0800

    add init

---
+++ src/main/java/com/zhangdxchn/dromespringbootdemo/DromeSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zhangdxchn.dromespringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DromeSpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DromeSpringbootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhangdxchn/dromespringbootdemo/DromeSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangdxchn.dromespringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DromeSpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
